e3b282566e811a8c7e91c3fae25ae015db445463
haisontop <haisontop@gmail.com>
2022-12-01 23:41:03 -0800

    fix testimonial parts

--- src/modules/about/Testimonials.tsx
+++ src/modules/about/Testimonials.tsx
@@ -15,48 +15,62 @@ import { OUR_STORY } from "../../constants";
 export default function Testimonials() {
   return (
     <Box width="100%" bgColor={"#F9F9F9"} py="4rem">
-      <Container width="100%" maxW={"80%"}>
+      <Container width="100%" maxW={"56rem"}>
         <Grid
           justifyContent={"space-around"}
           width="100%"
-          templateColumns={{base: "repeat(1, 1fr)", md: "repeat(2, 1fr)"}}
+          templateColumns={{
+            base: "repeat(1, 1fr)",
+            md: "repeat(2, 1fr)",
+            lg: "repeat(5, 1fr)",
+          }}
           gridRowGap="2rem"
         >
-          <Box display="flex" flexDir={"column"} justifyContent="center" order={{base: 2, md: 1}}>
-            <Text fontSize={"1.875rem"} fontWeight={700} pb="1.5rem">
-              Our Pod Loves Sonar
-            </Text>
-            <Box display={"flex"} alignItems="center">
-              <Box maxW={"90%"}>
-                <Text fontSize={"1.25rem"}>
-                  “Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Placerat elementum ultricies vel volutpat lacinia tellus
-                  mattis hac.”
-                </Text>
+          <GridItem colSpan={{ base: 1, lg: 3 }}>
+            <Box
+              display="flex"
+              flexDir={"column"}
+              justifyContent="center"
+              order={{ base: 2, md: 1 }}
+            >
+              <Text fontSize={"1.875rem"} fontWeight={700} pb="1.5rem">
+                Our Pod Loves Sonar
+              </Text>
+              <Box display={"flex"} alignItems="center">
+                <Box maxW={"90%"}>
+                  <Text fontSize={"1.125rem"}>
+                    “Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Placerat elementum ultricies vel volutpat lacinia tellus
+                    mattis hac.”
+                  </Text>
+                </Box>
+              </Box>
+              <Text fontWeight={600} sx={{ mt: "1rem" }}>
+                -Pod Fam
+              </Text>
+              <Box>
+                <Button
+                  variant={"outline"}
+                  colorScheme="primary"
+                  sx={{ mt: "1.875rem" }}
+                >
+                  Next Quote
+                </Button>
               </Box>
             </Box>
-            <Text fontWeight={600} sx={{ mt: "1rem" }}>
-              -Pod Fam
-            </Text>
-            <Box>
-              <Button
-                variant={"outline"}
-                colorScheme="primary"
-                sx={{ mt: "1.875rem" }}
-              >
-                Next Quote
-              </Button>
+          </GridItem>
+          <GridItem colSpan={{ base: 1, lg: 2 }}>
+            <Box order={{ base: 1, md: 2 }}>
+              <Image
+                src={"/images/girl-quote.png"}
+                width="100%"
+                alt="all-people"
+                maxH="327px"
+                // maxW={"327px"}
+                objectFit={"contain"}
+              ></Image>
             </Box>
-          </Box>
-          <Box order={{base: 1, md: 2}}>
-            <Image
-              src={"/images/girl-quote.png"}
-              width="100%"
-              alt="all-people"
-              maxH="327px"
-              objectFit={"contain"}
-            ></Image>
-          </Box>
+          </GridItem>
         </Grid>
       </Container>
     </Box>
